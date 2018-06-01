faf5d85d00e3ca5cdc6522d46a037575b92fbb16
Aaron Miller <millerac333@gmail.com>
2018-06-01 10:15:22 -0500

    first commit 1.2

---
+++ main.js
@@ -0,0 +1,64 @@
+const Lowes = Object.create({}, {
+    business: {
+        value: "Lowes Home Improvement"
+    },
+    employmentStart: {
+        value: "09-01-06"
+    },
+    employmentEnd: {
+        value: "06-01-2009"
+    },
+    position: {
+        value: "Customer Service Associate"
+    }
+
+})
+
+const MCVhospital = Object.create({}, {
+    business: {
+        value: "MCV Hospital"
+    },
+    employmentStart: {
+        value: "06-01-10"
+    },
+    employmentEnd: {
+        value: "08-01-2014"
+    },
+    position: {
+        enumerable: true,
+        writable: true,
+        value: "CNA"
+    }
+})
+
+const Hiller = Object.create({}, {
+    business: {
+        value: "Hiller Plumbing, Heating, Cooling, and Electrical"
+     },
+    employmentStart: {
+        value: "09-01-2014"
+     },
+    employmentEnd: {
+        value: "06-01-2017"
+     },
+    position: {
+        value: "Plumbing Technician"
+     }  
+})
+
+const Uber = Object.create({}, {
+    business: {
+        value: "Uber Rideshare"
+    },
+    employmentStart: {
+        value: "09-01-2014"
+    },
+    employmentEnd: {
+        enumerable: true,
+        writable: true,
+        value: "currently employed"
+    },
+    position: {
+        value: "Driver"
+    }
+})
