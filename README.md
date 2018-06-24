0db8b113311497a2af8d7ede24e8c49cf778e45d
Aaron Miller <millerac333@gmail.com>
2018-06-24 14:30:49 -0500

    added README.md and fixed function to complete challenge

---
+++ README.md
@@ -0,0 +1,33 @@
+PRACTICE
+Use Object.create() to define objects for every place that you've worked before NSS.
+
+For example:
+
+const ChickFilA = Object.create({}, {
+    business: {
+        value: "Chick-Fil-A"
+    },
+    employmentStart: {
+        value: "01-01-2009"
+    },
+    employmentEnd: {
+        value: "01-01-2010"
+    }
+})
+You add as many properties as you want for each job.
+
+Things you liked about the job
+Things you hated about the job
+etc...
+
+---------------------------------------------------------------------------------
+
+CHALLENGE
+Write a function that will build, and return, a job object. Make sure you define an argument for each one of the properties you want to be in the object.
+
+const JobMaker = (a,r,g,u,m,e,n,t,s) {
+    return theObjectYouBuilt
+}
+
+---------------------------------------------------------------------------------
+
--- main.js
+++ main.js
@@ -1,64 +1,102 @@
-const Lowes = Object.create({}, {
-    business: {
-        value: "Lowes Home Improvement"
-    },
-    employmentStart: {
-        value: "09-01-06"
-    },
-    employmentEnd: {
-        value: "06-01-2009"
-    },
-    position: {
-        value: "Customer Service Associate"
-    }
+////PRACTICE////
+// const Lowes = Object.create({}, {
+//     business: {
+//         value: "Lowes Home Improvement"
+//     },
+//     employmentStart: {
+//         value: "09-01-06"
+//     },
+//     employmentEnd: {
+//         value: "06-01-2009"
+//     },
+//     position: {
+//         value: "Customer Service Associate"
+//     }
 
-})
+// })
 
-const MCVhospital = Object.create({}, {
-    business: {
-        value: "MCV Hospital"
-    },
-    employmentStart: {
-        value: "06-01-10"
-    },
-    employmentEnd: {
-        value: "08-01-2014"
-    },
-    position: {
-        enumerable: true,
-        writable: true,
-        value: "CNA"
-    }
-})
+// const MCVhospital = Object.create({}, {
+//     business: {
+//         value: "MCV Hospital"
+//     },
+//     employmentStart: {
+//         value: "06-01-10"
+//     },
+//     employmentEnd: {
+//         value: "08-01-2014"
+//     },
+//     position: {
+//         enumerable: true,
+//         writable: true,
+//         value: "CNA"
+//     }
+// })
 
-const Hiller = Object.create({}, {
-    business: {
-        value: "Hiller Plumbing, Heating, Cooling, and Electrical"
-     },
-    employmentStart: {
-        value: "09-01-2014"
-     },
-    employmentEnd: {
-        value: "06-01-2017"
-     },
-    position: {
-        value: "Plumbing Technician"
-     }  
-})
+// const Hiller = Object.create({}, {
+//     business: {
+//         value: "Hiller Plumbing, Heating, Cooling, and Electrical"
+//      },
+//     employmentStart: {
+//         value: "09-01-2014"
+//      },
+//     employmentEnd: {
+//         value: "06-01-2017"
+//      },
+//     position: {
+//         value: "Plumbing Technician"
+//      }  
+// })
 
-const Uber = Object.create({}, {
-    business: {
-        value: "Uber Rideshare"
-    },
-    employmentStart: {
-        value: "09-01-2014"
-    },
-    employmentEnd: {
-        enumerable: true,
-        writable: true,
-        value: "currently employed"
-    },
-    position: {
-        value: "Driver"
-    }
-})
+// const Uber = Object.create({}, {
+//     business: {
+//         value: "Uber Rideshare"
+//     },
+//     employmentStart: {
+//         value: "09-01-2014"
+//     },
+//     employmentEnd: {
+//         enumerable: true,
+//         writable: true,
+//         value: "currently employed"
+//     },
+//     position: {
+//         value: "Driver"
+//     }
+// })
+
+//---------------------------------------------------------------------//
+
+//CHALLENGE//
+function Jobs (businessName, startDate, endDate, title) {
+    let jobsProtoype = Object.create({}, { 
+        business: {
+            value: businessName,
+            enumerable: true,
+            writable: true,
+        },
+        employmentStart: {
+            value: startDate,
+            enumerable: true,
+            writable: true,
+        },
+        employmentEnd:  {
+            value: endDate,
+            enumerable: true,
+            writable: true,   
+        },
+        position: {
+            value: title,
+            enumerable: true,
+            writable: true,     
+        }
+    })
+    return jobsProtoype;
+};
+
+var Hiller = Jobs("Hiller", "1/1/2014", "1/1/2017", "Plumbing Technician");
+
+
+console.log(Hiller);
+
+
+
